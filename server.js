1762dea8aabbe29b430f3fca55e8554dd7f3863f
Matt <something@gmail.com>
2019-09-22 15:56:56 -0400

    adding api/user server endpoint

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require("express");
-// to break apart response/requests
-const bodyParser = require("body-parser");
-// to interact with mongodb easier
 const mongoose = require("mongoose");
-
-// Routes
-const items = require("./routes/api/items");
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB (saved on MLab - https://cloud.mongodb.com/v2/5d8212bed5ec13595d6fad87#clusters/connect?clusterId=Cluster0)
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch(err => console.log(err));
 
 // Use Routes
-app.use("/api/items", items);
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+
 
 //be heard at either configured port such as on heroku etc or 5000
 const port = process.env.PORT || 5000;
