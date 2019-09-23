9eac50e41656081d01bcf4fdada70884fef8e09a
Matt <something@gmail.com>
2019-09-23 12:00:21 -0400

    auth guard to delete endpt

--- routes/api/items.js
+++ routes/api/items.js
@@ -28,8 +28,8 @@ router.post("/", auth, (req, res) => {
 
 // @route   DELETE api/items/:id
 // @desc    Delete An Item
-// @access  Public
-router.delete("/:id", (req, res) => {
+// @access  Private
+router.delete("/:id", auth, (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
